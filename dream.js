44bd7c4e76ad8b44007363147ee9be2a42a7bc47
Rordrie <152210227+Rordrie@users.noreply.github.com>
2024-01-07 19:36:42 +0100

    Add files via upload

---
+++ dream.js
@@ -0,0 +1,8 @@
+	const bar = document.querySelector('.menu')
+	const menu = document.querySelector('.lines').addEventListener('click',
+	function(e) {
+		if (bar.classList.contains('hide')){
+			bar.classList.remove('hide')
+
+	}else(bar.classList.add('hide'))
+		})
